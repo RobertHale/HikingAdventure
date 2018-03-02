e1ff04e3a01d87548dbbb44ec45769a4ea8ec3c8
alexdai186 <alexdai186@gmail.com>
2018-03-02 16:46:39 -0600

    added video scraper into service

--- scraper/scrapeService.py
+++ scraper/scrapeService.py
@@ -2,6 +2,39 @@ from fetch import fetchJSON, fetchXML
 from resort import Resort
 from trail import Trail
 
+import os
+
+import google.oauth2.credentials
+
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+# The CLIENT_SECRETS_FILE variable specifies the name of a file that contains
+# the OAuth 2.0 information for this application, including its client_id and
+# client_secret.
+CLIENT_SECRETS_FILE = "client_secret.json"
+
+# This OAuth 2.0 access scope allows for full read/write access to the
+# authenticated user's account and requires requests to use an SSL connection.
+SCOPES = ['https://www.googleapis.com/auth/youtube.force-ssl']
+API_SERVICE_NAME = 'youtube'
+API_VERSION = 'v3'
+
+def get_authenticated_service():
+  flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES)
+  credentials = flow.run_console()
+  return build(API_SERVICE_NAME, API_VERSION, credentials = credentials)
+
+def search_videos(keywords, num_results=1):
+	service = get_authenticated_service()
+	results = service.search().list(
+		part='snippet', 
+		maxResults=num_results, 
+		q=keywords, 
+		type='').execute()
+	print(results)
+
 """
 " service class for the scraper.
 " scrapes data on needed models
