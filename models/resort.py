7c03b1d7b2c55c296684cf9bf0415f7cd8852150
RobertHale <rthspike@gmail.com>
2018-03-06 19:17:47 -0600

    added youtube id to models

--- models/resort.py
+++ models/resort.py
@@ -1,42 +1,43 @@
 import sys
+
 sys.path.insert(0, '../database/')
 from sqlalchemy import Column, Integer, String, Float, ForeignKey
 from sqlalchemy.orm import relationship
 from database import Base
 from models import ResortTrailJunction
 
+
 class Resort(Base):
-
     __tablename__ = 'resorts'
-    id          = Column(Integer, primary_key=True)
-    name        = Column(String(50), unique=True)
-    lifts         = Column(Integer)
-    runs          = Column(Integer)
-    website       = Column(String(50), unique=True)
-    lat           = Column(Float)
-    lon           = Column(Float)
-    elev          = Column(Integer)
-    mapid         = Column(Integer)
-    mapurl        = Column(String(50), unique=True)
-    yelprating    = Column(Float)
-    reviewcount   = Column(Integer)
-    trails        = relationship('Trail', secondary='rtjunc', backref='trail_resorts')
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), unique=True)
+    lifts = Column(Integer)
+    runs = Column(Integer)
+    website = Column(String(50), unique=True)
+    lat = Column(Float)
+    lon = Column(Float)
+    elev = Column(Integer)
+    mapid = Column(Integer)
+    mapurl = Column(String(50), unique=True)
+    yelprating = Column(Float)
+    reviewcount = Column(Integer)
+    youtubeid = Column(String(50))
+    trails = relationship('Trail', secondary='rtjunc', backref='trail_resorts')
 
-    def __init__(self, name=None, id=None, lifts=None, runs=None, website=None, lat=None, lon=None, 
-                elev=None, mapid=None, mapurl=None, yelprating=None, reviewcount=None, trails=None):
-        self.name       = name
-        self.id         = id
-        self.lifts       = lifts
-        self.runs        = runs
-        self.website     = website
-        self.lat         = lat
-        self.lon         = lon
-        self.elev        = elev
-        self.mapid       = mapid
-        self.mapurl      = mapurl
-        self.yelprating  = yelprating
+    def __init__(self, name=None, id=None, lifts=None, runs=None, website=None, lat=None, lon=None,
+                 elev=None, mapid=None, mapurl=None, yelprating=None, reviewcount=None, trails=None):
+        self.name = name
+        self.id = id
+        self.lifts = lifts
+        self.runs = runs
+        self.website = website
+        self.lat = lat
+        self.lon = lon
+        self.elev = elev
+        self.mapid = mapid
+        self.mapurl = mapurl
+        self.yelprating = yelprating
         self.reviewcount = reviewcount
         if trails is None:
             trails = []
-        self.trails      = trails
-
+        self.trails = trails
--- models/trail.py
+++ models/trail.py
@@ -1,43 +1,44 @@
 import sys
+
 sys.path.insert(0, '../database/')
 from sqlalchemy import Column, Integer, String, Float, ForeignKey
 from sqlalchemy.orm import relationship
 from database import Base
 from models import ResortTrailJunction
 
+
 class Trail(Base):
+    __tablename__ = 'trails'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), unique=True)
+    difficulty = Column(String(50))
+    summary = Column(String(5000))
+    stars = Column(Float)
+    starVotes = Column(Integer)
+    lat = Column(Float)
+    lon = Column(Float)
+    length = Column(Float)
+    ascent = Column(Integer)
+    descent = Column(Integer)
+    img = Column(String(50))
+    youtubeid = Column(String(50))
+    resorts = relationship('Resort', secondary='rtjunc', backref='resort_trails')
 
-	__tablename__ = 'trails'
-	id            = Column(Integer, primary_key=True)
-	name          = Column(String(50), unique=True)
-	difficulty    = Column(String(50))
-	summary       = Column(String(5000))
-	stars         = Column(Float)
-	starVotes     = Column(Integer)
-	lat           = Column(Float)
-	lon           = Column(Float)
-	length        = Column(Float)
-	ascent        = Column(Integer)
-	descent       = Column(Integer)
-	img           = Column(String(50))
-	resorts       = relationship('Resort', secondary='rtjunc', backref='resort_trails')
-
-	def __init__(self, name=None, id=None, difficulty=None, summary=None, stars=None, 
-				starvotes=None, lat=None, lon=None, length=None, ascent=None, descent=None,
-				img=None, resorts=None):
-		self.name       = name
-		self.id         = id
-		self.difficulty = difficulty
-		self.summary    = summary
-		self.stars      = stars
-		self.starvotes  = starvotes
-		self.lat        = lat
-		self.lon        = lon
-		self.length     = length
-		self.ascent     = ascent
-		self.descent    = descent
-		self.img        = img
-		if resorts is None:
-			resorts=[]
-		self.resorts    = resorts
-		
+    def __init__(self, name=None, id=None, difficulty=None, summary=None, stars=None,
+                 starvotes=None, lat=None, lon=None, length=None, ascent=None, descent=None,
+                 img=None, resorts=None):
+        self.name = name
+        self.id = id
+        self.difficulty = difficulty
+        self.summary = summary
+        self.stars = stars
+        self.starvotes = starvotes
+        self.lat = lat
+        self.lon = lon
+        self.length = length
+        self.ascent = ascent
+        self.descent = descent
+        self.img = img
+        if resorts is None:
+            resorts = []
+        self.resorts = resorts
