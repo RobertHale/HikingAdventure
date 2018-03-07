204e674f70d244b256061d76d26f5f893e00d1ca
RobertHale <rthspike@gmail.com>
2018-03-06 19:28:31 -0600

    autopep8 formatting

--- models/photo.py
+++ models/photo.py
@@ -4,17 +4,17 @@ from sqlalchemy import Column, Integer, String, Float, ForeignKey
 from sqlalchemy.orm import relationship
 from database import Base
 
+
 class Photo(Base):
+    __tablename__ = 'photos'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), unique=True)
+    resortid = Column(Integer, ForeignKey("resorts.id"))
 
-	__tablename__ = 'photos'
-	id   = Column(Integer, primary_key=True)
-	name = Column(String(50), unique=True)
-	resortid = Column(Integer, ForeignKey("resorts.id"))
-
-	def __init__(self, name=None, id=None, resortid=None, photos=None):
-		self.name       = name
-		self.id         = id
-		self.resortid = resortid
-		if photos is None:
-			photos = []
-		self.photos = photos
+    def __init__(self, name=None, id=None, resortid=None, photos=None):
+        self.name = name
+        self.id = id
+        self.resortid = resortid
+        if photos is None:
+            photos = []
+        self.photos = photos
--- scraper/scrape.py
+++ scraper/scrape.py
@@ -1,4 +1,5 @@
 import sys
+
 sys.path.insert(0, '../models/')
 import scrapeService
 import json
@@ -9,34 +10,34 @@ import trail
 from complexhandler import ComplexHandler
 import pprint
 
+
 def main():
-	pp = pprint.PrettyPrinter(indent=4)
-	resorts, trails, photos = fullscrape(100, 10)
-	for photo in photos:
-		pp.pprint(photo.__dict__)
+    pp = pprint.PrettyPrinter(indent=4)
+    resorts, trails, photos = fullscrape(100, 10)
+    for photo in photos:
+        pp.pprint(photo.__dict__)
+
 
 def fullscrape(rcnt, tcnt):
-	"""
-	do a full scrape grabbing data for resorts,
-	trails, and photos.  
-	args:
-		rcnt = number of resorts to scrape
-		tcnt = number of trails to scrape per resort
-	"""
-	trails = {}
-	photos = []
-	print("Getting resorts: ", end="")
-	resorts = scrapeService.getResorts(rcnt)
-	print("Done\n")
-	print("Getting trails and photos: ", end="")
-	for resort in resorts:
-		photo = Photo(name=resort.getname(), id=resort.getid(), resortid=resort.getid())
-		trails, photo = scrapeService.getTrailsAndPhotos(resort.lat, resort.lon, tcnt, resort, trails, photo)
-		photos.append(photo)
-	print("Done\n")
-	return resorts, trails, photos
-
-
+    """
+    do a full scrape grabbing data for resorts,
+    trails, and photos.
+    args:
+        rcnt = number of resorts to scrape
+        tcnt = number of trails to scrape per resort
+    """
+    trails = {}
+    photos = []
+    print("Getting resorts: ", end="")
+    resorts = scrapeService.getResorts(rcnt)
+    print("Done\n")
+    print("Getting trails and photos: ", end="")
+    for resort in resorts:
+        photo = Photo(name=resort.getname(), id=resort.getid(), resortid=resort.getid())
+        trails, photo = scrapeService.getTrailsAndPhotos(resort.lat, resort.lon, tcnt, resort, trails, photo)
+        photos.append(photo)
+    print("Done\n")
+    return resorts, trails, photos
 
 
 if __name__ == "__main__":
