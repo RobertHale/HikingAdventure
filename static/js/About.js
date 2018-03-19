a91b4fe7328beb8e07f776f6b59d9337675451a0
davepcast <david.pedro.castilla@gmail.com>
2018-03-19 17:42:04 -0500

    about page done

--- static/js/About.js
+++ static/js/About.js
@@ -12,12 +12,42 @@ export default class About extends React.Component {
   constructor(){
     super();
     this.state = {
-      githubcont: [],
+      githubcont: [{"RobertHale":{
+          "commits": "",
+          "issues": "",
+          "tests": ""
+        },
+        "davepcast":{
+          "commits": "",
+          "issues": "",
+          "tests": ""
+        },
+        "duoALopez":{
+          "commits": "",
+          "issues": "",
+          "tests": ""
+        },
+        "alexdai186":{
+          "commits": "",
+          "issues": "",
+          "tests": ""
+        },
+        "victor40":{
+          "commits": "",
+          "issues": "",
+          "tests": ""
+        },
+        "vponakala":{
+          "commits": "",
+          "issues": "",
+          "tests": ""
+        }
+      }],
       githubstats: [{"total": ""},{"total": ""},{"total": ""},{"total": ""},{"total": ""},{"total": ""}],
       message: "",
-      totalcommits: 0,
-      totalissues: 0,
-      totaltests: 0
+      totalcommits: "",
+      totalissues: "",
+      totaltests: ""
     }
     this.changemessage = this.changemessage.bind(this);
     this.grabgitstats = this.grabgitstats.bind(this);
@@ -56,6 +86,7 @@ export default class About extends React.Component {
         "tests": 0
       }
     }];
+    var filler = [];
     let currentperson = '';
     $.getJSON('https://api.github.com/repos/RobertHale/HikingAdventure/stats/contributors').then(results => {
       results.map(individual => {
@@ -69,7 +100,8 @@ export default class About extends React.Component {
         githubcont: gitgroup
       });
     });
-    
+      $.getJSON('http://www.hikingadventures.me/api/trails').then(results => console.log(results));
+
   }
 
   changemessage(){
@@ -92,42 +124,44 @@ export default class About extends React.Component {
       <div>
 
       <Row className="justify-content-center">
-      <h4>About</h4>
+      <h4 className="title-text">About</h4>
+      <br/>
       </Row>
 
       <Row className="justify-content-center">
-      <h4>Team: SWEat Victory</h4>
+      <h4 className="title-text">Team: SWEat Victory</h4>
+      <br/>
       </Row>
 
       <Row className="justify-content-center">
 
-      <Col lg="4" md="6">
-      <img className="profile-picture" src="https://i.imgur.com/ZHPGrSu.jpg"/>
+      <Col lg="4" sm="8">
+      <img className="profile-picture img-thumbnail" src="https://i.imgur.com/ZHPGrSu.jpg"/>
       <p>
       <b>
-      {"Bio:"}
+      {"Bio: "}
       </b>
       {"I'm a 4th year Computer Science major at the University of Texas at Austin. In my free time I enjoy playing video games and meeting up with friends."}
       </p>
       <p>
-        <b>Responsibilities:</b> Frontend
+        <b>Responsibilities: </b> Frontend
       </p>
       <p>
-        <b># of Commits:</b>
+        <b># of Commits: </b> {this.state.githubcont[0].davepcast.commits}
       </p>
       <p>
-        <b># of Issues:</b> 1
+        <b># of Issues: </b> {this.state.githubcont[0].davepcast.issues}
       </p>
       <p>
-        <b># of Unit Tests:</b> 0
+        <b># of Unit Tests: </b> {this.state.githubcont[0].davepcast.tests}
       </p>
       </Col>
 
-      <Col lg="4" md="6">
-      <img className="profile-picture" src="https://i.imgur.com/4jHX00q.jpg"/>
+      <Col lg="4" sm="8">
+      <img className="profile-picture img-thumbnail" src="https://i.imgur.com/4jHX00q.jpg"/>
       <p>
       <b>
-      {"Bio:"}
+      {"Bio: "}
       </b>
       {"Hi! My name is Alex and I'm a Junior at the University of Texas at Austin, majoring in Computer Science. Some of my interests within Computer Science include Data Science and Machine Learning. In my free time, I enjoy fishing, playing basketball, playing poker, and hiking."}
       </p>
@@ -137,23 +171,23 @@ export default class About extends React.Component {
       </p>
 
       <p>
-        <b># of Commits:</b>{this.state.githubstats[1].total}
+        <b># of Commits: </b> {this.state.githubcont[0].alexdai186.commits}
       </p>
 
       <p>
-        <b># of Issues:</b> 1
+        <b># of Issues: </b> {this.state.githubcont[0].alexdai186.issues}
       </p>
 
       <p>
-        <b># of Unit Tests:</b> 0
+        <b># of Unit Tests: </b> {this.state.githubcont[0].alexdai186.tests}
       </p>
 
       </Col>
-      <Col lg="4" md="6">
-      <img className="profile-picture" src="https://i.imgur.com/mf7JSRj.jpg"/>
+      <Col lg="4" sm="8">
+      <img className="profile-picture img-thumbnail" src="https://i.imgur.com/mf7JSRj.jpg"/>
       <p>
       <b>
-      {"Bio:"}
+      {"Bio: "}
       </b>
       {"Hello, my name is Robert Hale and I'm a Junior at the University of Texas at Austin. I'm majoring in Computer Science with a minor in business. Some of my interests are in backend development and mobile applications. Outside of computer science, I compete in gymnastics and wakeboarding."}
       </p>
@@ -161,23 +195,23 @@ export default class About extends React.Component {
         <b>Responsibilities:</b> Backend
       </p>
       <p>
-        <b># of Commits:</b>{this.state.githubstats[3].total}
+        <b># of Commits: </b> {this.state.githubcont[0].RobertHale.commits}
       </p>
       <p>
-        <b># of Issues:</b> 1
+        <b># of Issues: </b> {this.state.githubcont[0].RobertHale.issues}
       </p>
       <p>
-        <b># of Unit Tests:</b> 0
+        <b># of Unit Tests: </b> {this.state.githubcont[0].RobertHale.tests}
       </p>
       </Col>
       </Row>
 
       <Row>
-      <Col lg="4" md="6">
-      <img className="profile-picture" src="https://i.imgur.com/F5Y7b3M.jpg"/>
+      <Col lg="4" sm="8">
+      <img className="profile-picture img-thumbnail" src="https://i.imgur.com/F5Y7b3M.jpg"/>
       <p>
       <b>
-      {"Bio:"}
+      {"Bio: "}
       </b>
       {"I'm a third year Computer Science student. I'm a fan of beautiful design and simplicity in programming and products. Outside of CS, I'm an amateur aquarist with experience breeding invertebrates."}
       </p>
@@ -185,20 +219,20 @@ export default class About extends React.Component {
         <b>Responsibilities:</b> Frontend
       </p>
       <p>
-        <b># of Commits:</b>{this.state.githubstats[0].total}
+        <b># of Commits: </b> {this.state.githubcont[0].duoALopez.tests}
       </p>
       <p>
-        <b># of Issues:</b> 1
+        <b># of Issues: </b> {this.state.githubcont[0].duoALopez.tests}
       </p>
       <p>
-        <b># of Unit Tests:</b> 0
+        <b># of Unit Tests: </b> {this.state.githubcont[0].duoALopez.tests}
       </p>
       </Col>
-      <Col lg="4" md="6">
-      <img className="profile-picture" src="https://i.imgur.com/00OjoLZ.jpg"/>
+      <Col lg="4" sm="8">
+      <img className="profile-picture img-thumbnail" src="https://i.imgur.com/00OjoLZ.jpg"/>
       <p>
       <b>
-      {"Bio:"}
+      {"Bio: "}
       </b>
       {"My name is Vamsi Ponakala, and I'm a junior at UT who is majoring in Computer Science. Some of my interests include learning about history, playing tennis and playing video games in my spare time."}
       </p>
@@ -206,21 +240,21 @@ export default class About extends React.Component {
         <b>Responsibilities:</b> Frontend
       </p>
       <p>
-        <b># of Commits:</b>
+        <b># of Commits: </b> {this.state.githubcont[0].vponakala.commits}
       </p>
       <p>
-        <b># of Issues:</b> 1
+        <b># of Issues: </b> {this.state.githubcont[0].vponakala.issues}
       </p>
       <p>
-        <b># of Unit Tests:</b> 0
+        <b># of Unit Tests: </b> {this.state.githubcont[0].vponakala.tests}
       </p>
       </Col>
 
-      <Col lg="4" md="6">
-      <img className="profile-picture" src="https://i.imgur.com/zkxl05U.jpg"/>
+      <Col lg="4" sm="8">
+      <img className="profile-picture img-thumbnail" src="https://i.imgur.com/zkxl05U.jpg"/>
       <p>
       <b>
-      {"Bio:"}
+      {"Bio: "}
       </b>
       {"I'm a senior at UT majoring in Computer Science expecting to graduate at the end of this semester. My hobbies include video games and board games, and I also enjoy traveling."}
       </p>
@@ -228,20 +262,49 @@ export default class About extends React.Component {
         <b>Responsibilities:</b> Frontend
       </p>
       <p>
-        <b># of Commits:</b>{this.state.githubstats[2].total}
+        <b># of Commits: </b> {this.state.githubcont[0].victor40.commits}
       </p>
       <p>
-        <b># of Issues:</b> 1
+        <b># of Issues: </b> {this.state.githubcont[0].victor40.issues}
       </p>
       <p>
-        <b># of Unit Tests:</b> 0
+        <b># of Unit Tests: </b> {this.state.githubcont[0].victor40.tests}
       </p>
       </Col>
-
       </Row>
 
-      <p>{this.state.totalcommits}</p>
-      {flow}
+      <br/>
+      <h3 className="title-text">Total Stats</h3>
+      <br/>
+      <p><b>Total Commits: </b></p>
+      <p><b>Total Issues: </b></p>
+      <p><b>Total Unit Tests: </b></p>
+      <br/>
+
+      <br/>
+      <h3 className="title-text">Sources</h3>
+      <br/>
+      <p><b>{"www.hikingproject.com/data"}</b>{" - provides API for trail info and photos"}</p>
+      <p><b>{"www.skimap.org"}</b>{" - provide API for ski resorts and their information"}</p>
+      <p><b>{"www.youtube.com"}</b>{" - providing API for displaying related youtube videos"}</p>
+      <br/>
+
+      <br/>
+      <h3 className="title-text">Tools</h3>
+      <br/>
+      <p><b>{"Bootstrap 4"}</b>{" - for beautifying and organizing HTML pages."}</p>
+      <p><b>{"Flask"}</b>{" - for the backend of the website"}</p>
+      <p><b>{"Github"}</b>{" - for maintaining our report and project organization"}</p>
+      <p><b>{"Slack"}</b>{" - for team communication and organization"}</p>
+      <p><b>{"Postman"}</b>{" - for organizing API calls"}</p>
+      <p><b>{"Amazon AWS"}</b>{" - for hosting website"}</p>
+      <br/>
+
+      <br/>
+      <p><b>{"Github: "}</b> <a href = "https://github.com/RobertHale/HikingAdventure">{"https://github.com/RobertHale/HikingAdventure"}</a></p>
+      <p><b>{"Gitbook: "}</b> <a href = "https://roberthale.gitbooks.io/api/">{"https://roberthale.gitbooks.io/report/"}</a></p>
+      <br/>
+
       </div>
     );
   }
