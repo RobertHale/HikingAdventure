bf3c43edb5ef935e3fbc96debf951e112974f40f
RobertHale <rthspike@gmail.com>
2018-03-28 22:40:05 -0500

    added form input fields

--- static/js/Rpopup.js
+++ static/js/Rpopup.js
@@ -11,22 +11,36 @@ export default class Rpopup extends React.Component {
         <ModalHeader 
           toggle={this.props.toggle}>Filter</ModalHeader>
         <ModalBody className='popup'>
-          <p># of lifts:</p>
           <InputGroup>
-            <InputGroupAddon className="text-white" addonType="prepend">Lower Bound: </InputGroupAddon>
+            <InputGroupAddon className="form-text" addonType="prepend"># of lifts >= </InputGroupAddon>
             <Input placeholder="GT"/>
-            <InputGroupAddon className="text-white" addonType="prepend"> Upper Bound: </InputGroupAddon>
-            <Input placeholder="LT"/>
           </InputGroup>
           <br></br>
           <InputGroup>
-            <InputGroupAddon className="text-white" addonType="prepend">Stars greater than: </InputGroupAddon>
+            <InputGroupAddon className="form-text" addonType="prepend"># of ski runs >= </InputGroupAddon>
+            <Input placeholder="GT"/>
+          </InputGroup>
+          <br></br>
+          <InputGroup>
+            <InputGroupAddon className="form-text" addonType="prepend">Elevation >= </InputGroupAddon>
+            <Input placeholder="GT"/>
+          </InputGroup>
+          <br></br>
+          <InputGroup>
+            <InputGroupAddon className="form-text" addonType="prepend">Stars greater than: </InputGroupAddon>
             <Input placeholder="Stars"/>
+          </InputGroup>
+          <br></br>
+          <InputGroup>
+            <InputGroupAddon className="form-text" addonType="prepend">Lat:</InputGroupAddon>
+            <Input placeholder="latitude"/>
+            <InputGroupAddon className="form-text" addonType="prepend">Long:</InputGroupAddon>
+            <Input placeholder="longitude"/>
           </InputGroup>
         </ModalBody>
         <ModalFooter>
           <Button color="primary">Submit</Button>
-          <Button color="secondary" onClick={this.props.toggle}>close me</Button>
+          <Button color="secondary" onClick={this.props.toggle}>Cancel</Button>
         </ModalFooter>
       </Modal>
     );
