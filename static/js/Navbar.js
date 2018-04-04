04af4bc8c8fbc149b058119700f1a8decb9117a7
davepcast <david.pedro.castilla@gmail.com>
2018-04-03 21:11:01 -0500

    minor css changes

--- static/js/Navbar.js
+++ static/js/Navbar.js
@@ -14,7 +14,7 @@ import {
   DropdownItem,
   Button,
   FormGroup,
-  input
+  Input
 } from 'reactstrap';
 import {withRouter} from 'react-router-dom'
 
@@ -67,7 +67,7 @@ class NavBar extends React.Component {
       </Nav>
       </Collapse>
       <FormGroup className="form-inline ml-auto" expand="md">
-      <input className="form-control mr-sm-2" type="search" placeholder="Search" value={this.state.sInput} onChange={this.newInput} aria-label="Search" />
+      <Input className="form-control mr-sm-2" type="search" placeholder="Search" value={this.state.sInput} onChange={this.newInput} aria-label="Search" />
       <Button outline color="light" type="submit" onClick={this.submitSearch}>Search</Button>
       </FormGroup>
       </Navbar>
@@ -77,24 +77,3 @@ class NavBar extends React.Component {
 }
 
 export default withRouter(NavBar);
-
-// <nav class="navbar navbar-toggleable-md sticky-top navbar-inverse" style="background-color: #212121;">
-//   <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-//     <span class="navbar-toggler-icon"></span>
-//   </button>
-//   <a class="navbar-brand" href="http://hikingadventures.me">Home</a>
-//   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-//     <div class="navbar-nav">
-//       <a class="nav-item nav-link" href="http://hikingadventures.me/resorts">Resorts<span class="sr-only">(current)</span></a>
-//       <a class="nav-item nav-link" href="http://hikingadventures.me/trails">Trails</a>
-//       <a class="nav-item nav-link" href="http://hikingadventures.me/photos">Photos</a>
-//       <a class="nav-item nav-link" href="http://hikingadventures.me/about">About</a>
-//     </div>
-//   </div>
-//   <form class="form-inline my-2 my-sm-0 w-40">
-//     <div class="input-group">
-//       <input class="form-control mr-sm-0" type="text" placeholder="">
-//       <button class="btn btn-outline-secondary my-2 my-sm-0" type="submit">Search</button>
-//     </div>
-//   </form>
-// </nav>
