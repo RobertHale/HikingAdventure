49a68766d41b9f2076f4be74b6f195928f476eaa
davepcast <david.pedro.castilla@gmail.com>
2018-04-14 18:51:52 -0500

    resultspage/mainpages/iframe loading updated

---
+++ static/assets/Pages.js
@@ -0,0 +1,129 @@
+// App.jsx
+import React from "react";
+import {
+  Button,
+  Pagination,
+  PaginationItem,
+  PaginationLink
+ } from 'reactstrap';
+import { Link } from "react-router-dom";
+
+export default class Pages extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      filler: []
+    }
+  }
+  render () {
+    // Grab tota/current page information, url as well
+    var totalPages = this.props.pagedata.pagecount;
+    var currentPage= this.props.pagedata.cpage;
+    var url = this.props.pagedata.url;
+
+    // adjusted url for corresponding pagination button
+    var aurl = '';
+
+    // array that we loop through to generate page buttons
+    var pages = [];
+    // 0 means no pagiation is needed, 1 otherwise
+    var exists = 0;
+    // holds pagination number buttons i.e. 1,2,3,4,5
+    let items = "";
+    // holds pagination prev/next button
+    let prev = "";
+    let next = "";
+
+    // by default prev/next buttons are not disabled
+    aurl = url
+    aurl += (currentPage - 1);
+    prev =  <PaginationItem>
+            <Link className="page-link" to={aurl}>
+            {"«"}
+            </Link>
+            </PaginationItem>
+
+    aurl = url
+    aurl += (currentPage + 1);
+    next =  <PaginationItem>
+            <Link className="page-link" to={aurl}>
+            {"»"}
+            </Link>
+            </PaginationItem>
+
+    // check if prev/next should be disabled
+    if (currentPage == 1){
+      prev =  <PaginationItem disabled>
+              <Link className="page-link" to="">
+              {"«"}
+              </Link>
+              </PaginationItem>
+    }
+    if (currentPage == totalPages){
+      next =  <PaginationItem disabled>
+              <Link className="page-link" to="">
+              {"»"}
+              </Link>
+              </PaginationItem>
+    }
+
+    // loading pagination in sets of 5, handle case if less than 5
+    if(totalPages < 5){
+      for(var x = 0; x < totalPages; x++){
+        pages.push(x+1);
+      }
+    }
+    // handles creating pagination when near total pages
+    else if((currentPage + 2) >= totalPages){
+      var length = totalPages - 4;
+      for(length; length <= totalPages; length++){
+        pages.push(length);
+      }
+    }
+    // handles creating pagination when near first page
+    else if((currentPage - 2) <= 0){
+      pages = [1,2,3,4,5];
+    }
+    else{
+      pages = [currentPage-2, currentPage-1, currentPage, currentPage+1, currentPage+2];
+    }
+    // generate pagination number buttons
+    if(pages.length != 0){
+      exists = 1
+      items = pages.map(page => {
+        aurl = url;
+        aurl += page;
+        if(currentPage == page){
+          return(
+            <PaginationItem disabled key={page}>
+            <Link className="page-link" to={aurl}>
+            {page}
+            </Link>
+            </PaginationItem>
+          );
+        }
+        else{
+          return(
+            <PaginationItem key={page}>
+            <Link className="page-link" to={aurl}>
+            {page}
+            </Link>
+            </PaginationItem>
+          );
+        }
+      })
+    }
+
+    return (
+      <div>
+      {exists == 1 ?
+        <Pagination>
+        {prev}
+        {items}
+        {next}
+        </Pagination>
+         : null}
+      </div>
+      );
+  }
+}
--- static/js/Resorts.js
+++ static/js/Resorts.js
@@ -16,6 +16,7 @@ import ResortRow from "./ResortRow";
 import Rpopup from "./Rpopup";
 import NavBar from "./Navbar";
 import Pages from "./Pages";
+import Spinner from "./Spinner";
 import $ from 'jquery';
 
 export default class Resorts extends React.Component {
@@ -36,7 +37,8 @@ export default class Resorts extends React.Component {
       showPopup: false,
       showSorting: 0,
       showDirection: 0,
-      filter: ""
+      filter: "",
+      loading: true
     }
     this.toggle = this.toggle.bind(this);
     this.pairup = this.pairup.bind(this);
@@ -86,12 +88,14 @@ export default class Resorts extends React.Component {
     this.setState({
       presorts: paired,
       pagecount: Math.ceil(resultcount/10),
-      cpage: pagenumber
+      cpage: pagenumber,
+      loading: false
     });
   }
   //This is where we want to query the database
   //For now we use temporary information
   componentWillReceiveProps(nextProps){
+    this.setState({loading: true});
     window.scrollTo(0, 0)
     var id = ("\"id\"");
     var dir = ("\"" + this.state.dirList[this.state.showDirection] + "\"");
@@ -105,7 +109,8 @@ export default class Resorts extends React.Component {
       pagenumber = temp[1];
       pagenumber = parseInt(pagenumber, 10);
     }
-    var url = "http://hikingadventures.me/api/resorts?q={";
+    //http://127.0.0.1:5000/api/resorts?page=
+    var url = "http://127.0.0.1:5000/api/resorts?q={";
     url += "\"order_by\":[";
     if (this.state.sortBy != this.state.sortEnum.NONE) {
       url += "{\"field\":\"" + this.state.sortList[this.state.sortBy] + "\"";
@@ -120,6 +125,7 @@ export default class Resorts extends React.Component {
   }
 
   componentDidMount(){
+      this.setState({loading: true});
       var pagenumber = this.props.match.params.page;
       var id = ("\"id\"");
       var dir = ("\"" + this.state.dirList[this.state.showDirection] + "\"");
@@ -133,147 +139,147 @@ export default class Resorts extends React.Component {
         pagenumber = parseInt(pagenumber, 10);
       }
 
-      var fetchfrom = "http://hikingadventures.me/api/resorts?page=";
+      var fetchfrom = "http://127.0.0.1:5000/api/resorts?page=";
       fetchfrom += pagenumber;
-
-
       $.getJSON(fetchfrom).then(results => {this.pairup(results.objects, results.num_results, pagenumber)});
-    }
-    componentWillUnmount(){
-      //Testing purposes
-    }
-
-    sort(sort, dir){
-        var pagenumber = this.props.match.params.page;
-        var temp;
-        if(pagenumber == null){
-          pagenumber = 1
-        }
-        else{
-          temp = pagenumber.split(" ");
-          pagenumber = temp[1];
-          pagenumber = parseInt(pagenumber, 10);
-        }
-        var url = "http://hikingadventures.me/api/resorts?q=";
-        url += "{\"order_by\":[";
-        if (sort != this.state.sortEnum.NONE) {
-          url += "{\"field\":\"" + this.state.sortList[sort] + "\"";
-          url += ",\"direction\":\"" + this.state.dirList[dir] + "\"}";
-        }
-        url += "]";
-        if(!(this.state.filter === "")) url += "," + this.state.filter;
-        url += "}";
-        url += "&page="
-        url += pagenumber
-        $.getJSON(url).then(results => {this.pairup(results.objects, results.num_results, pagenumber)});
-    }
-
-    clickedLift(){
-      this.setState({sortBy: this.state.sortEnum.LIFTS});
-      this.setState({showSorting: this.state.sortEnum.LIFTS});
-      this.sort(this.state.sortEnum.LIFTS, this.state.direction);
-    }
-
-    clickedElev(){
-      this.setState({sortBy: this.state.sortEnum.ELEV});
-      this.setState({showSorting: this.state.sortEnum.ELEV});
-      this.sort(this.state.sortEnum.ELEV, this.state.direction);
-    }
-
-    clickedName(){
-      this.setState({sortBy: this.state.sortEnum.NAME});
-      this.setState({showSorting: this.state.sortEnum.NAME});
-      this.sort(this.state.sortEnum.NAME, this.state.direction);
-    }
-
-    clickedStars(){
-      this.setState({sortBy: this.state.sortEnum.STARS});
-      this.setState({showSorting: this.state.sortEnum.STARS});
-      this.sort(this.state.sortEnum.STARS, this.state.direction);
-    }
-
-    clickedRuns(){
-      this.setState({sortBy: this.state.sortEnum.RUNS});
-      this.setState({showSorting: this.state.sortEnum.RUNS});
-      this.sort(this.state.sortEnum.RUNS, this.state.direction);
-    }
-
-    clickedReview(){
-      this.setState({sortBy: this.state.sortEnum.REVIEW});
-      this.setState({showSorting: this.state.sortEnum.REVIEW});
-      this.sort(this.state.sortEnum.REVIEW, this.state.direction);
-    }
-
-    clickedDesc(){
-      this.setState({direction: this.state.dirEnum.DESC});
-      this.setState({showDirection: this.state.dirEnum.DESC});
-      this.sort(this.state.sortBy, this.state.dirEnum.DESC);
-    }
-
-    clickedAsc(){
-      this.setState({direction: this.state.dirEnum.ASC});
-      this.setState({showDirection: this.state.dirEnum.ASC});
-      this.sort(this.state.sortBy, this.state.dirEnum.ASC);
-    }
-
-
-    render () {
-      let rrow;
-      if(this.state.presorts){
-        rrow = this.state.presorts.map(currentc => {
-          return(
-            <ResortRow key={currentc[0].id} data={currentc} />
-          );
-        })
-      }
-      return(
-        <div>
-        <NavBar/>
-        <Container>
-        <Row>
-        <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
-        <DropdownToggle color="primary" caret>
-          Sort by: {this.state.sortList[this.state.showSorting]}
-        </DropdownToggle>
-        <DropdownMenu>
-          <DropdownItem onClick={this.clickedName}>Name</DropdownItem>
-          <DropdownItem divider/>
-          <DropdownItem onClick={this.clickedLift}>Lifts</DropdownItem>
-          <DropdownItem divider/>
-          <DropdownItem onClick={this.clickedElev}>Elevation</DropdownItem>
-          <DropdownItem divider/>
-          <DropdownItem onClick={this.clickedStars}>Yelp Rating</DropdownItem>
-          <DropdownItem divider/>
-          <DropdownItem onClick={this.clickedRuns}>Runs</DropdownItem>
-          <DropdownItem divider/>
-          <DropdownItem onClick={this.clickedReview}>Review Count</DropdownItem>
-        </DropdownMenu>
-        </Dropdown>
-        <Dropdown isOpen={this.state.btnDropup} toggle={() => { this.setState({ btnDropup: !this.state.btnDropup}); }}>
-        <DropdownToggle color="primary" caret>
-        Direction: {this.state.dirList[this.state.showDirection]}
-        </DropdownToggle>
-        <DropdownMenu>
-        <DropdownItem onClick={this.clickedAsc}>Ascending</DropdownItem>
-        <DropdownItem divider/>
-        <DropdownItem onClick={this.clickedDesc}>Descending</DropdownItem>
-        </DropdownMenu>
-        </Dropdown>
-        <Button color="primary" onClick={this.togglePopup.bind(this)}>Filter</Button>
-        </Row>
-        {rrow}
-        <br/>
-        <Row className="justify-content-center">
-        <Pages pagedata={{pagecount: this.state.pagecount, url: "/resortspage= ", cpage: this.state.cpage}}/>
-        </Row>
-        <Rpopup
-            text='Close Me'
-            isOpen={this.state.showPopup}
-            toggle={this.togglePopup.bind(this)}
-            submit={this.submitFilter}
-          />
-        </Container>
-        </div>
-      );
-    }
   }
+
+  componentWillUnmount(){
+      //Testing purposes
+  }
+
+  sort(sort, dir){
+    this.setState({loading: true});
+    var pagenumber = this.props.match.params.page;
+    var temp;
+    if(pagenumber == null){
+      pagenumber = 1
+    }
+    else{
+      temp = pagenumber.split(" ");
+      pagenumber = temp[1];
+      pagenumber = parseInt(pagenumber, 10);
+    }
+    var url = "http://127.0.0.1:5000/api/resorts?q=";
+    url += "{\"order_by\":[";
+    if (sort != this.state.sortEnum.NONE) {
+      url += "{\"field\":\"" + this.state.sortList[sort] + "\"";
+      url += ",\"direction\":\"" + this.state.dirList[dir] + "\"}";
+    }
+    url += "]";
+    if(!(this.state.filter === "")) url += "," + this.state.filter;
+      url += "}";
+      url += "&page="
+      url += pagenumber
+      $.getJSON(url).then(results => {this.pairup(results.objects, results.num_results, pagenumber)});
+  }
+
+  clickedLift(){
+    this.setState({sortBy: this.state.sortEnum.LIFTS});
+    this.setState({showSorting: this.state.sortEnum.LIFTS});
+    this.sort(this.state.sortEnum.LIFTS, this.state.direction);
+  }
+
+  clickedElev(){
+    this.setState({sortBy: this.state.sortEnum.ELEV});
+    this.setState({showSorting: this.state.sortEnum.ELEV});
+    this.sort(this.state.sortEnum.ELEV, this.state.direction);
+  }
+
+  clickedName(){
+    this.setState({sortBy: this.state.sortEnum.NAME});
+    this.setState({showSorting: this.state.sortEnum.NAME});
+    this.sort(this.state.sortEnum.NAME, this.state.direction);
+  }
+
+  clickedStars(){
+    this.setState({sortBy: this.state.sortEnum.STARS});
+    this.setState({showSorting: this.state.sortEnum.STARS});
+    this.sort(this.state.sortEnum.STARS, this.state.direction);
+  }
+
+  clickedRuns(){
+    this.setState({sortBy: this.state.sortEnum.RUNS});
+    this.setState({showSorting: this.state.sortEnum.RUNS});
+    this.sort(this.state.sortEnum.RUNS, this.state.direction);
+  }
+
+  clickedReview(){
+    this.setState({sortBy: this.state.sortEnum.REVIEW});
+    this.setState({showSorting: this.state.sortEnum.REVIEW});
+    this.sort(this.state.sortEnum.REVIEW, this.state.direction);
+  }
+
+  clickedDesc(){
+    this.setState({direction: this.state.dirEnum.DESC});
+    this.setState({showDirection: this.state.dirEnum.DESC});
+    this.sort(this.state.sortBy, this.state.dirEnum.DESC);
+  }
+
+  clickedAsc(){
+    this.setState({direction: this.state.dirEnum.ASC});
+    this.setState({showDirection: this.state.dirEnum.ASC});
+    this.sort(this.state.sortBy, this.state.dirEnum.ASC);
+  }
+
+  render () {
+    let rrow;
+    let isloading = this.state.loading;
+    if(this.state.presorts){
+      rrow = this.state.presorts.map(currentc => {
+        return(
+          <ResortRow key={currentc[0].id} data={currentc} />
+        );
+      })
+    }
+    return(
+      <div>
+      <NavBar/>
+      <Container>
+      <Row>
+      <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+      <DropdownToggle color="primary" caret>
+        Sort by: {this.state.sortList[this.state.showSorting]}
+      </DropdownToggle>
+      <DropdownMenu>
+        <DropdownItem onClick={this.clickedName}>Name</DropdownItem>
+        <DropdownItem divider/>
+        <DropdownItem onClick={this.clickedLift}>Lifts</DropdownItem>
+        <DropdownItem divider/>
+        <DropdownItem onClick={this.clickedElev}>Elevation</DropdownItem>
+        <DropdownItem divider/>
+        <DropdownItem onClick={this.clickedStars}>Yelp Rating</DropdownItem>
+        <DropdownItem divider/>
+        <DropdownItem onClick={this.clickedRuns}>Runs</DropdownItem>
+        <DropdownItem divider/>
+        <DropdownItem onClick={this.clickedReview}>Review Count</DropdownItem>
+      </DropdownMenu>
+      </Dropdown>
+      <Dropdown isOpen={this.state.btnDropup} toggle={() => { this.setState({ btnDropup: !this.state.btnDropup}); }}>
+      <DropdownToggle color="primary" caret>
+      Direction: {this.state.dirList[this.state.showDirection]}
+      </DropdownToggle>
+      <DropdownMenu>
+      <DropdownItem onClick={this.clickedAsc}>Ascending</DropdownItem>
+      <DropdownItem divider/>
+      <DropdownItem onClick={this.clickedDesc}>Descending</DropdownItem>
+      </DropdownMenu>
+      </Dropdown>
+      <Button color="primary" onClick={this.togglePopup.bind(this)}>Filter</Button>
+      </Row>
+      {isloading ? <Spinner/> : rrow}
+      <br/>
+      <Row className="justify-content-center">
+      <Pages pagedata={{pagecount: this.state.pagecount, url: "/resortspage= ", cpage: this.state.cpage}}/>
+      </Row>
+      <Rpopup
+          text='Close Me'
+          isOpen={this.state.showPopup}
+          toggle={this.togglePopup.bind(this)}
+          submit={this.submitFilter}
+        />
+      </Container>
+      </div>
+      );
+  }
+}
