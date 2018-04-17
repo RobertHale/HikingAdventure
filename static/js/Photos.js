2aa8131bf9255f78ace8f972cbcd8dff22675aa2
RobertHale <rthspike@gmail.com>
2018-04-17 16:03:27 -0500

    aligned photo page and added contents to page.

--- static/js/Photos.js
+++ static/js/Photos.js
@@ -134,7 +134,7 @@ export default class Photos extends React.Component {
       pagenumber = parseInt(pagenumber, 10);
     }
     //console.log(pagenumber);
-    let fetchfrom = "http://hikingadventures.me/api/photos?page=";
+    let fetchfrom = "http://127.0.0.1:5000/api/photos?page=";
     fetchfrom += pagenumber;
     //console.log(fetchfrom);
     $.getJSON(fetchfrom).then(results => {this.pairup(results.objects, results.num_results, pagenumber)});
@@ -156,7 +156,7 @@ export default class Photos extends React.Component {
       pagenumber = temp[1];
       pagenumber = parseInt(pagenumber, 10);
     }
-    let url = "http://hikingadventures.me/api/photos?q=";
+    let url = "http://127.0.0.1:5000/api/photos?q=";
     url += "{\"order_by\":[";
     if (field != this.state.sortEnum.NONE) {
       url += "{\"field\":\"" + this.state.sortList[field] + "\"";
